3dae99c14d98f55438cc0877c8a1e1f513de6aea
lev2454 <lewartamonov@gmail.com>
2021-04-28 17:27:30 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <random>
 #include "libs/stegaxor.h"
+#include "libs/keygen.h"
 
 
 using namespace std;
@@ -14,6 +15,7 @@ int main(int argc, char const *argv[]) {
 	if (argc != 2)
 		exit(-1);
 
+	generate(string("xor.key"));
     encrypt_data(argv[1], string("output"), string("xor.key"));
     decrypt_file(string("output"), string("dec.txt"), string("xor.key"));
 
